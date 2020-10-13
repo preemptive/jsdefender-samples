bf1316018d2ed65526e1628a688597c1d4f79fac
Victor Brown <therealvictorbrown@gmail.com>
2020-10-13 18:10:22 +0200

    feat: add self-defending demo

---
+++ self-defending/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const {
+  JSDefenderWebpackPlugin,
+} = require("@preemptive/jsdefender-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/main.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devtool: "source-map",
+  mode: "development",
+
+  plugins: [
+    new JSDefenderWebpackPlugin({
+      configurationFile: "../jsdefender.config.json",
+      quietMode: false,
+      enableInDevelopmentMode: false,
+    }),
+  ],
+};
